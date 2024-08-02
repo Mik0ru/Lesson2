767df61b557f7b3d070dfebcb84ea2ec4300fc05
Mik0ru <aziztermecikov5@gmail.com>
2024-08-03 01:08:52 +0600

    Optimised person variable

--- src/HW2.java
+++ src/HW2.java
@@ -1,16 +1,16 @@
 import java.util.Random;
 public class HW2 {
     public static void main(String[] args) {
-        String person1 = permission(generateRandomAge(),30);
-        System.out.println(person1);
-        String person2 = permission(generateRandomAge(),20);
-        System.out.println(person2);
-        String person3 = permission(generateRandomAge(),15);
-        System.out.println(person3);
-        String person4 = permission(generateRandomAge(),40);
-        System.out.println(person4);
-        String person5 = permission(generateRandomAge(),-20);
-        System.out.println(person5);
+        String person = permission(generateRandomAge(),30);
+        System.out.println(person);
+         person = permission(generateRandomAge(),20);
+        System.out.println(person);
+         person = permission(generateRandomAge(),15);
+        System.out.println(person);
+         person = permission(generateRandomAge(),40);
+        System.out.println(person);
+         person = permission(generateRandomAge(),-20);
+        System.out.println(person);
 
     }
     public static String permission (int personAge, int temperature) {
